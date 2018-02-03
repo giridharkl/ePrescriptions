098b694800123f200d6d06ed106966a804ad7281
giridharkl <giridharkl@gmail.com>
2018-02-03 21:25:38 +0200

    missing Id field added

--- src/main/java/com/eprescriptions/store/Store.java
+++ src/main/java/com/eprescriptions/store/Store.java
@@ -1,10 +1,15 @@
 package com.eprescriptions.store;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
 @Entity
 @SequenceGenerator(sequenceName="store_seq", initialValue=1, name="store_seq")
 public class Store {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="store_seq")
+	Long Id;
 }
